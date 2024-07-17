f3c071c992459722d68203eb4228f8b24277a64b
killuazulldik <arielabellana10012002@gmail.com>
2024-07-17 15:08:35 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import Student from "./Student";
+
 function App() {
   return (
-    <div className="App">
-      <h1>My First React App Installation</h1>
-      <h2>What is React?</h2>
-      <p>
-        React is a free and open-source front-end JavaScript library for
-        building user interfaces <br />
-        based on components by Facebook Inc. It is maintaine by Meta and a
-        community of <br />
-        individual developers and companies. React can be used to develop
-        single-page, mobile, or <br />
-        server-rendered applications with framework like Next.js.
-      </p>
+    <>
+      {/* <div className="App">
+        <h1>My First React App Installation</h1>
+        <h2>What is React?</h2>
+        <p>
+          React is a free and open-source front-end JavaScript library for
+          building user interfaces <br />
+          based on components by Facebook Inc. It is maintaine by Meta and a
+          community of <br />
+          individual developers and companies. React can be used to develop
+          single-page, mobile, or <br />
+          server-rendered applications with framework like Next.js.
+        </p>
 
-      <p>
-        1. Deiparine, Caryll Jean C. <br />
-        2. Abellana, Ariel L.
-      </p>
-    </div>
+        <p>
+          1. Deiparine, Caryll Jean C. <br />
+          2. Abellana, Ariel L.
+        </p>
+      </div> */}
+      <Student />
+    </>
   );
 }
 
